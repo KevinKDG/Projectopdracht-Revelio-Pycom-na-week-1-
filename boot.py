61933e14cd5b24782046fdc296b1ee765ee9142c
KevinKDG <72076173+KevinKDG@users.noreply.github.com>
2021-02-20 15:49:15 +0100

    added Boot.py

--- boot.py
+++ boot.py
@@ -1,24 +1,24 @@
-from machine import UART  # Universal synchronous receiver / transmitter
+import pycom
+from network import WLAN
+import machine
+import time
 
+# ---- Config ----
 
+wlan = WLAN(mode=WLAN.STA)
+pycom.heartbeat(False)
+wlan.connect(ssid='ssid', auth=(WLAN.WPA2, 'password!'))
 
-uart = UART(bits=8, baudrate=9600, parity=None, stop=1, timeout_chars=100 pins('P3', 'P4'))
-# bits = number of bits per character 5,6,7 or 8
-# baudrate = clock rate
-# parity = check error
-# stop = number of stop bits 1 or 2
-# timeout_chars = RX timeout in number of characters
-# pins = pin 3 & 4 are our RX and TX pins on the microcontroller
+# ---- Loop ----
 
-while True:
-    header_byte = uart.read(1)
-    while header_byte != b'\XFF'):
-        header_byte = uart.read(1)
+while not wlan.isconnected():
+    time.sleep(2)
+    pycom.rgbled(0xFF0000)
+    print("no connection")
+    machine.idle()
 
-    DATA_H = int(uart.read(1)[0])
-    Data_L = int(uart.read(1)[0])
-    SUM = int(uart.read(1)[0])
-
-    if DATA_H + DATA_L == SUM
-        distance = (Data_H*256)+ Data_L
-        print(distance)
+while wlan.isconnected():
+    print("WiFi connected succesfully")
+    time.sleep(1)
+    print(wlan.ifconfig())
+    pycom.rgbled(0x00FF00)
