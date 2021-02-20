7c65a86a784ffd121ceaa6c68226564b85819350
KevinKDG <72076173+KevinKDG@users.noreply.github.com>
2021-02-20 15:24:44 +0100

    Added time of flight sensor data

---
+++ boot.py
@@ -0,0 +1,17 @@
+import time
+
+import board
+import busio
+
+import adafruit_vl53l0x
+
+# Initialize I2C for SCL & SDA pins
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+
+
+# Print the range every second
+while True:
+    print("Range: {0}mm".format(vl53.range))
+    time.sleep(1.0)
